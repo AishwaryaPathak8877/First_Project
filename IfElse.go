92598a566a0f0f8f97eccbb80bb754bdf584e77c
AishwaryaPathak8877 <82440908+AishwaryaPathak8877@users.noreply.github.com>
2021-04-13 15:39:02 +0530

    If Else Program in Go

---
+++ IfElse.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main(){
+  
+  num:=0
+  if num < 0{
+    fmt.Println("Number is even")
+  }else if num > 0{
+    fmt.Println("Number is even")
+  }else{
+    fmt.Println("Number is zero")
+  }

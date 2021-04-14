8c0aeaa3e30e19459f81df3db474a44d5e154b42
AishwaryaPathak8877 <82440908+AishwaryaPathak8877@users.noreply.github.com>
2021-04-14 10:41:41 +0530

    Arrays in Go

---
+++ ArrayExample.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main(){
+  myarray[5]int
+  
+  myarray[0] = 1
+  myarray[1] = 2
+  myarray[2] = 3
+  myarray[3] = 4
+  myarray[4] = 5
+  
+  fmt.Println(myarray[0])
+  fmt.Println(myarray[1])
+  fmt.Println(myarray[2])
+  fmt.Println(myarray[3])
+  fmt.Println(myarray[4])
+}

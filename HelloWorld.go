f5d50948cf8d055cc4f468ccc7255c5fdc938e62
AishwaryaPathak8877 <82440908+AishwaryaPathak8877@users.noreply.github.com>
2021-04-14 11:39:17 +0530

    Update HelloWorld.go

--- HelloWorld.go
+++ HelloWorld.go
@@ -1,13 +1,13 @@
 package main
 
-import {
-  "fmt"
-  "time"
-}
+import (
+ "fmt"
+ "time"
+)
 
 func main(){
   fmt.Println("Hello World")
-  newtime := time.newtime(5*time.second)
+  newtime := time.NewTimer(5*time.Second)
   <-newtime.C
   fmt.Println("After 5 Seconds")
  }
